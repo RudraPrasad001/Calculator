29ddea5c5f127049c11111c1e3b63f70d1ba7765
RudraPrasad001 <rudraprasad.email@gmail.com>
2024-11-12 19:16:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const el = document.getElementById('display');
+var scientific = document.getElementById('Scientific');
+var  vis = false;
+function append(key){
+    el.value+=key;
+}
+function cl(){
+    el.value=" ";
+}
+function eq(){
+    try{
+    el.value=eval(el.value);
+    if(el.value=="Infinity"||el.value=="undefined"){
+        throw new Error(er);
+    }
+}
+    catch(error){
+        el.value="Error";
+        el.classList.add('red');
+        setTimeout(()=>{
+            
+            el.value=" ";},1000);
+        setTimeout(()=>{el.classList.remove('red');},1000);
+    }
+}
+function scin(){
+    if(!vis){
+        scientific.classList.add('anim');
+        scientific.classList.remove('upward');
+        scientific.classList.remove('hid');
+        vis = true;
+    }
+    else{
+        scientific.classList.remove('anim');
+        scientific.classList.add('upward');
+        setTimeout(()=>{scientific.classList.add('hid');},1000);
+        
+        vis=false;
+    }
+}
